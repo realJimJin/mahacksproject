c9061147b1b71a34442791d5d0cd6c46ded221c0
JonCoombs <jonathan.d.coombs@gmail.com>
2019-10-26 18:38:40 -0400

    Character Movement Added

---
+++ Assets/Scripts/MovePlayer1.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer1 : MonoBehaviour
+{
+    public bool Grounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.5f);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.5f);
+        }
+
+        if(Grounded && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 500, 0));
+            Grounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            Grounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            Grounded = false;
+    }
+}
