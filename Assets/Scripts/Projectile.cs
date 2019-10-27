8fa82dc7695fbc5a030ff51c77b38eed354bf44c
s-leirbag <gabriel@shiu.us>
2019-10-26 20:48:01 -0400

    projectile basics

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Rigidbody rb;
+    public double speed = 0.01;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward * (float)speed, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        
+    }
+}
